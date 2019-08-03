83a8b9953e1c9187bb7deb93271644566eb3f5d8
kuvotu <kuvotu@armail.in>
2019-08-03 22:14:30 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+$(window).on("load", function () {
+    $(".loader-wrapper").fadeOut("slow");
+});
+
+if (window.innerHeight >= 1024 && (window.innerWidth <= 1024)) {
+    $("#hero img").css({ "height": "33vh" });
+}
+
+$(document).ready(function () {
+    var img, oldImg, newImg;
+    $("#hero a img").click(function () {
+        img = $(this)[0];
+        oldImg = $(".modal-content img")[0];
+        newImg = new Image();
+        if (oldImg !== undefined) {
+            if (img.src === oldImg.src) {
+
+            } else {
+                newImg.src = img.src;
+                $(".modal-content img").replaceWith(newImg);
+            }
+        } else {
+            newImg.src = img.src;
+            $(".modal-content").append(newImg);
+        }
+    });
+
+});
